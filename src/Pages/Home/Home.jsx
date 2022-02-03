921a4d7fc6857a86030526f4788697af17d343c3
raghmala <rmalagari@teksystems.com>
2022-02-03 21:47:49 +0530

    Home page initial commit

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import AppPage from "./Components/AppPage/AppPage";
+
+function Home() {
+  return (
+    <div className="App">
+      <AppPage>
+        <h2>Table comes here</h2>
+      </AppPage>
+    </div>
+  );
+}
+
+export default Home;
