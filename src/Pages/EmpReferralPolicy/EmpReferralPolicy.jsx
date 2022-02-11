ff7fbda7ffd1fac7e1c56df0052b505d36a7ba1f
saikrishna28 <saikrishnasmarty@gmail.com>
2022-02-11 23:20:16 +0530

    adding referral policy

--- src/Pages/EmpReferralPolicy/EmpReferralPolicy.jsx
+++ src/Pages/EmpReferralPolicy/EmpReferralPolicy.jsx
@@ -1,10 +1,141 @@
 import React from "react";
 import AppPage from "../../Components/AppPage/AppPage";
+import "./EmpReferralPolicy.scss";
 
 function EmpReferralPolicy() {
   return (
     <div>
-      <AppPage title="Employee Referral Policy"></AppPage>
+      <AppPage title="Employee Referral Policy">
+        <p>
+          <span className="bold">Overview: </span>TEKsystems Global Services
+          (TGS) practices participatory culture at work & this policy gives an
+          opportunity for employees to participate in the growth of the
+          organization and get rewarded for the same. This is in turn gives
+          responsibility to the employees to make the right choice while
+          referring their friends & acquaintances for hiring. While employees at
+          TGS are brand ambassadors of the company, they would also be
+          accountable to ensure that individual(s) they refer gel well with the
+          culture of the organization.
+        </p>
+        <h4>Eligibility:</h4>
+        <p>
+          ❖ Full-time employees & Direct contract employees working in the
+          Organization may refer a candidate who is known to him / her
+          personally / professionally
+        </p>
+        <p>
+          ❖ Referral of candidates having experience less than two (2) years
+          would not be considered for referral payouts unless specifically
+          requested and published by Recruitment team.
+        </p>
+        <p>
+          ❖ The candidate shall meet all the requirements of the position, in
+          terms of qualifications, work experience, skill set etc.
+        </p>
+        <p>
+          ❖ The employee, referring a candidate for the position, shall send the
+          CV to the email id mentioned below with a reference letter. (See below
+          for format of reference letter).
+        </p>
+        <p>
+          ❖ The employee shall not canvas or interfere in any way in selection
+          of the candidate.
+        </p>
+        <p>❖ All open positions are open for Employee Referral.</p>
+        <p>
+          ❖ Recruitment team members and Leadership team including Associate
+          Directors are excluded from the Employee Referral benefits.
+        </p>
+        <p>
+          ❖ Recruitment team members and Leadership team including Associate
+          Directors are excluded from the Employee Referral benefits.
+        </p>
+        <p>❖ Rehire cases will not be eligible for the referral scheme.</p>
+        <p>
+          ❖ Immediate family members are excluded from the Employee Referral
+          benefits
+        </p>
+        <p>
+          ❖ Employees hired for contractual/contractual conversion are excluded
+          from the Employee Referral benefits
+        </p>
+        <h4>Benefits:</h4>
+        <table width="400px">
+          <tr>
+            <th>#</th>
+            <th>Years of Experience</th>
+            <th>Referral Bonus</th>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>0-2 years</td>
+            <td>NA</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>2-5 years</td>
+            <td>INR 10,000/-</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>5+ years</td>
+            <td>INR 20,000/-</td>
+          </tr>
+        </table>
+        <p>
+          If there are five or more references from an employee who join in a
+          quarter, the employee will be eligible for an additional bonus of INR
+          25,000/-
+        </p>
+        <h4>Terms and Conditions:</h4>
+        <p>
+          Should the referred candidate join the organization, the following
+          terms & conditions will apply for the referral reward amount to be
+          paid.
+        </p>
+        <p>
+          ❖ The referral candidate completes three (3) months of continuous
+          service with the organization.
+        </p>
+        <p>
+          ❖ Referral resume must be sent to bsc_careers@teksystems.com for
+          Bangalore positions and hsc_careers@teksystems.com for Hyderabad
+          positions. Without the email, referrals will not be considered.
+        </p>
+        <p>
+          ❖ At the time of referral payout, both the candidate and the employee
+          should be in active service and should not be serving his/her notice
+          period.
+        </p>
+        <p>❖ Referral reward amount is paid as Bonus and is taxable.</p>
+        <p>
+          ❖ There is no upper limit on the number of candidates each employee
+          can refer.
+        </p>
+        <p>
+          ❖ Employee who has referred the candidate should not be involved in
+          any aspect of the recruitment process of the candidate.
+        </p>
+        <p>
+          ❖ If two or more employees refer the same candidate, the referral
+          reward amount would be paid to the employee who referred the candidate
+          first based on timestamp receipt of email.
+        </p>
+        <p>
+          ❖ If the referred candidate is interviewed and gets rejected, and the
+          same candidate is referred by another employee after 3 months and gets
+          selected and joins TGS; the referral reward amount would be paid to
+          the employee who had last referred.
+        </p>
+        <p>
+          ❖ All referred resumes submitted by the employees will be valid for 3
+          months only.
+        </p>
+        <p>
+          ❖ In case of any compulsions for the recruitment of any candidate, the
+          concerned employee shall discuss with the Recruitment Manager.
+        </p>
+      </AppPage>
     </div>
   );
 }
